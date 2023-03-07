e0296f1bc4a42d58fe253a5b75dc4c905cd48fdd
mateusbrebal <mateus.brebal@gmail.com>
2023-03-06 21:31:09 -0300

    Impressão Parcial do Resultado dos alunos

--- UC9_GIT_MEDIA_PORCENTAGEM/Program.cs
+++ UC9_GIT_MEDIA_PORCENTAGEM/Program.cs
@@ -1,8 +1,11 @@
 ﻿string[,] alunos = new string[5, 7];
 int l, c;
+double media, faltas;
 
 l = 0;
 c = 0;
+media = 0;
+faltas = 0;
 
 for (l = 0; l <= 4; l++)
 {
@@ -20,4 +23,13 @@ for (l = 0; l <= 4; l++)
     alunos[l, c++] = Console.ReadLine();
     Console.WriteLine("Nota da TRABALHO:");
     alunos[l, c++] = Console.ReadLine();
+
+    c = 0;
+}
+
+for (l = 0; l <=4; l++)
+{
+    media = (double.Parse(alunos[l, 3]) * 0.3) + (double.Parse(alunos[l, 4]) * 0.25) + (double.Parse(alunos[l, 5]) * 0.35) + double.Parse(alunos[l, 6]);
+    faltas = (double.Parse(alunos[l, 2]) * 100) / double.Parse(alunos[l, 1]);
+
 }
