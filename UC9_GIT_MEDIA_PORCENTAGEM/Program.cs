656b611e7cee5adfb7f926554cc831b07e18ab92
mateusbrebal <mateus.brebal@gmail.com>
2023-03-06 21:41:55 -0300

    Finalização da aplicação

--- UC9_GIT_MEDIA_PORCENTAGEM/Program.cs
+++ UC9_GIT_MEDIA_PORCENTAGEM/Program.cs
@@ -31,5 +31,20 @@ for (l = 0; l <=4; l++)
 {
     media = (double.Parse(alunos[l, 3]) * 0.3) + (double.Parse(alunos[l, 4]) * 0.25) + (double.Parse(alunos[l, 5]) * 0.35) + double.Parse(alunos[l, 6]);
     faltas = (double.Parse(alunos[l, 2]) * 100) / double.Parse(alunos[l, 1]);
-
+    if (media >= 6 && faltas <= 25)
+    {
+        Console.WriteLine("APROVADO com MÉDIA: " + media.ToString("F") + " e " + faltas.ToString("F") + "% de FALTAS");
+    }
+    if (media < 6 && faltas <= 25)
+    {
+        Console.WriteLine("REPROVADO por MÉDIA: " + media.ToString("F"));
+    }
+    if (media >= 6 && faltas > 25)
+    {
+        Console.WriteLine("REPROVADO por FALTAS: " + faltas.ToString("F"));
+    }
+    if (media < 6 && faltas > 25)
+    {
+        Console.WriteLine("REPROVADO com MÉDIA: " + media.ToString("F") + " e " + faltas.ToString("F") + "% de FALTAS");
+    }
 }
